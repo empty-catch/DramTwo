ae45460f5074e3f3cc56c10252811037588fd40b
Jeon Sehun <twilivez@gmail.com>
2020-10-08 00:43:58 +0900

    [ADD] MonsterGestureResource 관리 해 줄 Singleton 스크립테이블 추가
    Mosnter Gesture Resource 를 Dictionary 로 관리

---
+++ Assets/01.Scripts/Monster/MonsterGestureResources.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class MonsterGestureResource {
+    // TODO : string enum으로 변경
+    [SerializeField]
+    private string name;
+    public string Name => name;
+    
+    [SerializeField]
+    private Sprite gestureSprite;
+    public Sprite GestureSprite => gestureSprite;
+}
+
+[CreateAssetMenu(fileName = "MonsterGestureSprite", menuName = "Resources/MonsterGestureSprite",order = 0)]
+public class MonsterGestureResources : ScriptableObject {
+    private static string path = "/Unit/Resource/MonsterGestureSprite";
+    private static MonsterGestureResources _instance;
+
+    public static MonsterGestureResources instance {
+        get {
+            if (_instance == null) {
+                var temp = Resources.Load<MonsterGestureResources>(path);
+                if (temp == null) {
+                    throw new Exception("Not Found Monster Gesture Sprite Resources.");
+                }
+
+                _instance = temp;
+            }
+            
+            return _instance;
+        }
+    }
+
+    [Header("Gesture Items")]
+    [SerializeField]
+    private MonsterGestureResource[] monsterGestureResources;
+    
+    // TODO : string enum으로 변경
+    private Dictionary<string, Sprite> gestureItems = new Dictionary<string, Sprite>();
+    public Dictionary<string, Sprite> GestureItems => gestureItems;
+    
+    private void Awake() {
+        foreach (var resourceItem in monsterGestureResources) {
+            if (gestureItems.ContainsKey(resourceItem.Name) == false) {
+                gestureItems.Add(resourceItem.Name ,resourceItem.GestureSprite);
+            }
+        }
+    }
+}
