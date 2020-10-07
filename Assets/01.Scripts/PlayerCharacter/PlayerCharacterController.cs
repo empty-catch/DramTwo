501c09d46d41ad36b2e06e69eda8eb61f5787bfe
Jeon Sehun <twilivez@gmail.com>
2020-10-07 23:47:10 +0900

    [ADD] GetComponentSafe 추가 및 PlayerCharacterController 수정

--- Assets/01.Scripts/PlayerCharacter/PlayerCharacterController.cs
+++ Assets/01.Scripts/PlayerCharacter/PlayerCharacterController.cs
@@ -1,20 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCharacterController : MonoBehaviour {
     private static PlayerCharacterController _instance;
-
     public static PlayerCharacterController instance {
         get {
             if (_instance == null) {
-                var instanceObject = GameObject.FindObjectOfType<PlayerCharacterController>().GetComponent<PlayerCharacterController>();
+                var instanceObject = GameObject.FindObjectOfType<PlayerCharacterController>();
 
                 if (instanceObject == null) {
                     var prefabs = Resources.Load<GameObject>("/Unit/PlayerCharacter");
                     var playerCharacterObject = Instantiate(prefabs, Vector2.zero, Quaternion.identity);
 
-                    instanceObject = playerCharacterObject.AddComponent<PlayerCharacterController>();
+                    instanceObject = playerCharacterObject.GetComponentSafe<PlayerCharacterController>();
                 }
 
                 _instance = instanceObject;
---
+++ Assets/01.Scripts/System/Extensions.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public static class Extensions {
+    public static T GetComponentSafe<T>(this Transform transform) where T : MonoBehaviour {
+        var component = transform.GetComponent<T>();
+        if (component != null) {
+            transform.gameObject.AddComponent<T>();
+        }
+
+        return component;
+    }        
+    
+    public static T GetComponentSafe<T>(this GameObject gameObject) where T : MonoBehaviour {
+        var component = gameObject.GetComponent<T>();
+        if (component != null) {
+            gameObject.AddComponent<T>();
+        }
+
+        return component;
+    }    
+}
