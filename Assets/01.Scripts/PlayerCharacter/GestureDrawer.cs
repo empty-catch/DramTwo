53671c470f0cf58fbc222faf2a92446d6940f685
Park Changyong <tempus157@gmail.com>
2020-10-09 00:04:51 +0900

    [UPDATE] 제스처 페이드 효과 및 핸들 추가

--- Assets/01.Scripts/PlayerCharacter/GestureDrawer.cs
+++ Assets/01.Scripts/PlayerCharacter/GestureDrawer.cs
@@ -1,19 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using PDollarGestureRecognizer;
+﻿using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
+using PDollarGestureRecognizer;
 
 public class GestureDrawer : MonoBehaviour {
-    [SerializeField] private new LineRenderer renderer;
-    [SerializeField] private TextAsset[] xmls;
+    [SerializeField]
+    private new LineRenderer renderer;
 
-    private Gesture[] gestures;
+    [SerializeField]
+    private Image handle;
+
+    [SerializeField]
+    private float fadeDuration;
+
+    [SerializeField]
+    private TextAsset[] xmls;
+
     private readonly List<Point> points = new List<Point>();
-    private Vector3 position;
-    private int strockID = -1;
+    private new Camera camera;
+    private Gesture[] gestures;
     private int positionCount;
 
+    private Tweener rendererTweener;
+    private Tweener handleTweener;
+
     private void Awake() {
+        camera = Camera.main;
         gestures = new Gesture[xmls.Length];
         for (var i = 0; i < xmls.Length; i++) {
             gestures[i] = GestureIO.ReadGestureFromXML(xmls[i].text);
@@ -22,28 +35,55 @@ public class GestureDrawer : MonoBehaviour {
 
     private void Update() {
         if (Input.GetMouseButtonDown(0)) {
-            strockID++;
-            renderer.gameObject.SetActive(true);
-            positionCount = 0;
+            Enable();
         }
         else if (Input.GetMouseButton(0)) {
-            position = Input.mousePosition;
-            points.Add(new Point(position.x, -position.y, strockID));
-            positionCount++;
-            renderer.positionCount = positionCount;
-        }
-        else if (Input.GetMouseButtonUp(0)) {
-            if (positionCount > 10 || (positionCount > 1 &&
-                                       (renderer.GetPosition(0) - renderer.GetPosition(positionCount - 1))
-                                       .sqrMagnitude >= 0.5f)) {
-                var candidate = new Gesture(points.ToArray());
-                var result = PointCloudRecognizer.Classify(candidate, gestures);
+            var position = Input.mousePosition;
+            var worldPoint = camera.ScreenToWorldPoint(new Vector3(position.x, position.y, 10));
+            if (renderer.positionCount > 0 && worldPoint == renderer.GetPosition(positionCount - 1)) {
+                return;
             }
 
-            strockID = -1;
-            points.Clear();
-            renderer.positionCount = 0;
-            renderer.gameObject.SetActive(false);
+            points.Add(new Point(position.x, -position.y, 0));
+            positionCount++;
+            renderer.positionCount = positionCount;
+            renderer.SetPosition(positionCount - 1, worldPoint);
+            handle.rectTransform.position = worldPoint;
         }
+        else if (Input.GetMouseButtonUp(0)) {
+            // if (positionCount > 10 || positionCount > 1 &&
+            //     (renderer.GetPosition(0) - renderer.GetPosition(positionCount - 1)).sqrMagnitude >= 0.5f) {
+            //     var candidate = new Gesture(points.ToArray());
+            //     var result = PointCloudRecognizer.Classify(candidate, gestures);
+            // }
+
+            Disable();
+        }
+    }
+
+    private void Enable() {
+        points.Clear();
+        positionCount = 0;
+        renderer.positionCount = 0;
+
+        rendererTweener?.Kill();
+        renderer.enabled = true;
+        renderer.startColor = renderer.endColor = Color.white;
+
+        handle.enabled = true;
+        handleTweener?.Kill();
+        handle.color = Color.white;
+    }
+
+    private void Disable() {
+        var rendererColor = renderer.startColor;
+        var transparentColor = rendererColor * new Color(1f, 1f, 1f, 0f);
+        var startColor2 = new Color2(rendererColor, rendererColor);
+        var endColor2 = new Color2(transparentColor, transparentColor);
+
+        rendererTweener = renderer.DOColor(startColor2, endColor2, fadeDuration)
+                                  .OnComplete(() => renderer.enabled = false);
+        handleTweener = handle.DOFade(0f, fadeDuration)
+                              .OnComplete(() => handle.enabled = false);
     }
 }
