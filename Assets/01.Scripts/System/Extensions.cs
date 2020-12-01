912eade2733add4e40bddb29f82f205e4b417e23
Jeon Sehun <twilivez@gmail.com>
2020-12-01 22:33:29 +0900

    Added submodule Assets/Unity-Library

--- Assets/01.Scripts/System/Extensions.cs
+++ Assets/01.Scripts/System/Extensions.cs
@@ -3,24 +3,6 @@ using DG.Tweening;
 using UnityEngine;
 
 public static class Extensions {
-    public static T GetComponentSafe<T>(this Transform transform) where T : MonoBehaviour {
-        var component = transform.GetComponent<T>();
-        if (component == null) {
-            transform.gameObject.AddComponent<T>();
-        }
-
-        return component;
-    }
-
-    public static T GetComponentSafe<T>(this GameObject gameObject) where T : MonoBehaviour {
-        var component = gameObject.GetComponent<T>();
-        if (component == null) {
-            gameObject.AddComponent<T>();
-        }
-
-        return component;
-    }
-
     // ReSharper disable once InconsistentNaming
     public static Tweener DOFade(this LineRenderer renderer, float endValue, float duration) {
         var ca = renderer.startColor;
