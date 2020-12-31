2f19474458d77481960b2cb23289f0a6f3567c05
Park Changyong <tempus157@gmail.com>
2020-12-31 18:16:34 +0900

    [UPDATE] 하트 스킬 예외 추가

--- Assets/01.Scripts/Skill/HeartSkill.cs
+++ Assets/01.Scripts/Skill/HeartSkill.cs
@@ -1,9 +1,18 @@
 ﻿using UnityEngine;
 
 public class HeartSkill : ISkill {
+    private const int SpCost = 3;
     public GestureType GestureType => GestureType.Heart;
-    
+
     public void Activate() {
+        if (PlayerCharacterController.Instance.Sp < SpCost) {
+            throw new NotEnoughSpException("The player character's SP is not enough");
+        }
+
+        if (PlayerCharacterController.Instance.IsFullHp) {
+            throw new SkillCannotBeUsedException("Heart skill cannot be used when player character is full hp");
+        }
+
         PlayerCharacterController.Instance.Hp++;
     }
 }
---
+++ Assets/01.Scripts/Skill/SkillCannotBeUsedException.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+public class SkillCannotBeUsedException : Exception {
+    public SkillCannotBeUsedException() { }
+    public SkillCannotBeUsedException(string message) : base(message) { }
+    public SkillCannotBeUsedException(string message, Exception inner) : base(message, inner) { }
+}
