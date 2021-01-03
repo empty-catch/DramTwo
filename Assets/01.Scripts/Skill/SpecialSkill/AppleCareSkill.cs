85ace8d23f285c05a211c6525e06293669ea50ec
Park Changyong <tempus157@gmail.com>
2021-01-03 18:41:18 +0900

    [ADD[ 특수 스킬 클래스 추가

---
+++ Assets/01.Scripts/Skill/SpecialSkill/AppleCareSkill.cs
@@ -0,0 +1,7 @@
+﻿public class AppleCareSkill : ISpecialSKill {
+    public int RequireLevel => 3;
+
+    public void Activate() {
+        // TODO: HP 1.5칸 획득, 이후 오는 공격 1회 방어
+    }
+}
---
+++ Assets/01.Scripts/Skill/SpecialSkill/SlaughterSkill.cs
@@ -0,0 +1,7 @@
+﻿public class SlaughterSkill : ISpecialSKill {
+    public int RequireLevel => 1;
+
+    public void Activate() {
+        // TODO: 모든 몬스터 제거
+    }
+}
