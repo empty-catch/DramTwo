b36207baf50ea1d11df6bba2c5bbca53ed850e75
Park Changyong <tempus157@gmail.com>
2021-01-03 17:35:55 +0900

    [ADD] 특수 스킬 사용 조건 추가 준비
    - 30초 쿨타임
    - 기본 모양 100번

--- Assets/01.Scripts/Skill/SkillHandler.cs
+++ Assets/01.Scripts/Skill/SkillHandler.cs
@@ -1,8 +1,12 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class SkillHandler : MonoBehaviour {
     private readonly Dictionary<GestureType, ISkill> skills = new Dictionary<GestureType, ISkill>();
+    private readonly Stopwatch cooldown = new Stopwatch();
+
+    private int specialSkillLevel = 0;
 
     public void Activate(GestureType gestureType) {
         skills[gestureType]?.Activate();
