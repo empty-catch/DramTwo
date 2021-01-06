b779f9b1df007122050fa4cdb4b6ad9cd17fcd38
Park Changyong <tempus157@gmail.com>
2021-01-06 16:32:20 +0900

    [UPDATE] Apple Care 스킬 레벨 2로 변경

--- Assets/01.Scripts/Skill/SpecialSkill/AppleCareSkill.cs
+++ Assets/01.Scripts/Skill/SpecialSkill/AppleCareSkill.cs
@@ -1,5 +1,5 @@
 ﻿public class AppleCareSkill : ISpecialSkill {
-    public int RequireLevel => 3;
+    public int RequireLevel => 2;
 
     public void Activate() {
         // TODO: HP 1.5칸 획득, 이후 오는 공격 1회 방어
