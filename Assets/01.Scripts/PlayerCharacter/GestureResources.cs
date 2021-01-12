622a972eedd9c084e7e36558513606862c715aed
Park Changyong <tempus157@gmail.com>
2021-01-12 18:14:09 +0900

    [ADD] 제스처 리소스 SO 추가

---
+++ Assets/01.Scripts/PlayerCharacter/GestureResources.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Gesture Resources", menuName = "Scriptable Object/Gesture Resources", order = 0)]
+public class GestureResources : ScriptableObject {
+    #region Singleton
+
+    private static MonsterGestureResources instance;
+
+    public static MonsterGestureResources Instance {
+        get {
+            if (instance != null) {
+                return instance;
+            }
+
+            instance = Resources.Load<MonsterGestureResources>("Unit/Resource/");
+            return instance;
+        }
+    }
+
+    #endregion
+
+    [SerializeField]
+    private Info[] infos;
+
+    private Dictionary<GestureType, Sprite> items = new Dictionary<GestureType, Sprite>();
+
+    public IEnumerable<GestureType> AllGestures => items.Keys;
+    public Sprite Sprite(GestureType gesture) => items[gesture];
+
+    private void OnEnable() {
+        foreach (var info in infos) {
+            items[info.gesture] = info.sprite;
+        }
+    }
+
+    private struct Info {
+        public GestureType gesture;
+        public Sprite sprite;
+    }
+}
