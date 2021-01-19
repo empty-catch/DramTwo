a6a86b3d65d844ce00b443aa3fd63419d5179e34
Park Changyong <tempus157@gmail.com>
2021-01-19 16:21:08 +0900

    [UPDATE] 제스처 모양 머리 위 에 뜨도록

---
+++ Assets/01.Scripts/Context/GestureContext.cs
@@ -0,0 +1,17 @@
+﻿using Slash.Unity.DataBind.Core.Data;
+using UnityEngine;
+
+public class GestureContext : Context {
+    private readonly Property<bool> activeProperty = new Property<bool>();
+    private readonly Property<Sprite> spriteProperty = new Property<Sprite>();
+
+    public bool Active {
+        get => activeProperty.Value;
+        set => activeProperty.Value = value;
+    }
+
+    public Sprite Sprite {
+        get => spriteProperty.Value;
+        set => spriteProperty.Value = value;
+    }
+}
--- Assets/01.Scripts/Context/PlayerCharacterContext.cs
+++ Assets/01.Scripts/Context/PlayerCharacterContext.cs
@@ -5,22 +5,16 @@ using UnityEngine;
 
 public class PlayerCharacterContext : Context {
     private readonly Property<float> timerFillProperty = new Property<float>();
-
-    private readonly Property<Collection<bool>>
-        activesProperty = new Property<Collection<bool>>(new Collection<bool>());
-
-    private readonly Property<Collection<Sprite>> spritesProperty =
-        new Property<Collection<Sprite>>(new Collection<Sprite>());
+    private readonly Property<GestureContext> gesture0Property = new Property<GestureContext>(new GestureContext());
+    private readonly Property<GestureContext> gesture1Property = new Property<GestureContext>(new GestureContext());
+    private readonly Property<GestureContext> gesture2Property = new Property<GestureContext>(new GestureContext());
+    private readonly Property<GestureContext> gesture3Property = new Property<GestureContext>(new GestureContext());
+    private readonly Property<GestureContext> gesture4Property = new Property<GestureContext>(new GestureContext());
 
     public PlayerCharacterContext() {
-        for (var i = 0; i < PlayerCharacterController.Instance.GestureCount; i++) {
-            Actives.Add(false);
-            Sprites.Add(null);
-        }
-
         PlayerCharacterController.Instance.TimerFilled += fillAmount => TimerFill = fillAmount;
-        PlayerCharacterController.Instance.GestureActiveSet += (index, active) => Actives[index] = active;
-        PlayerCharacterController.Instance.GestureSpriteSet += (index, sprite) => Sprites[index] = sprite;
+        PlayerCharacterController.Instance.GestureActiveSet += (index, active) => Gesture(index).Active = active;
+        PlayerCharacterController.Instance.GestureSpriteSet += (index, sprite) => Gesture(index).Sprite = sprite;
     }
 
     public float TimerFill {
@@ -28,6 +22,39 @@ public class PlayerCharacterContext : Context {
         private set => timerFillProperty.Value = value;
     }
 
-    public Collection<bool> Actives => activesProperty.Value;
-    public Collection<Sprite> Sprites => spritesProperty.Value;
+    public GestureContext Gesture0 {
+        get => gesture0Property.Value;
+        private set => gesture0Property.Value = value;
+    }
+
+    public GestureContext Gesture1 {
+        get => gesture1Property.Value;
+        private set => gesture1Property.Value = value;
+    }
+
+    public GestureContext Gesture2 {
+        get => gesture2Property.Value;
+        private set => gesture2Property.Value = value;
+    }
+
+    public GestureContext Gesture3 {
+        get => gesture3Property.Value;
+        private set => gesture3Property.Value = value;
+    }
+
+    public GestureContext Gesture4 {
+        get => gesture4Property.Value;
+        private set => gesture4Property.Value = value;
+    }
+
+    private GestureContext Gesture(int index) {
+        return index switch {
+            0 => Gesture0,
+            1 => Gesture1,
+            2 => Gesture2,
+            3 => Gesture3,
+            4 => Gesture4,
+            _ => null
+        };
+    }
 }
