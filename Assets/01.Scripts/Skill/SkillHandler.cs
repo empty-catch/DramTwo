2b9ce0f05b558bc1171331d13bdc8d9ee2c1a310
Park Changyong <tempus157@gmail.com>
2021-01-23 16:42:27 +0900

    [UPDATE] 레벨에 따라 특수 스킬 사요

--- Assets/01.Scripts/Skill/SkillHandler.cs
+++ Assets/01.Scripts/Skill/SkillHandler.cs
@@ -11,7 +11,6 @@ public class SkillHandler : MonoBehaviour {
     private readonly Dictionary<GestureType, ISkill> skills = new Dictionary<GestureType, ISkill>();
     private readonly Dictionary<int, ISkill> specialSkills = new Dictionary<int, ISkill>();
 
-    private int specialSkillLevel;
     private int gestureUsageCount;
     private bool canActivateSpecialSkill = true;
 
@@ -34,7 +33,7 @@ public class SkillHandler : MonoBehaviour {
     }
 
     private void ApplySpecialSkill() {
-        for (var i = 0; i < specialSkillLevel + 1; i++) {
+        for (var i = 0; i < SpecialSkillLevel.Value + 1; i++) {
             if (specialSkills.TryGetValue(i, out var skill)) {
                 skill.Activate();
             }
---
+++ Assets/01.Scripts/Skill/SpecialSkillLevel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+public class SpecialSkillLevel : MonoBehaviour {
+    private static readonly int[] levelUpRequirements = new int[] {150, 50, 50};
+    private static int levelUpProgress;
+
+    private static int LevelUpRequirement => levelUpRequirements[Value];
+    public static int Value { get; private set; }
+
+    public static void UpdateLevel() {
+        levelUpProgress++;
+
+        if (levelUpProgress >= LevelUpRequirement) {
+            levelUpProgress = 0;
+            Value++;
+        }
+    }
+}
